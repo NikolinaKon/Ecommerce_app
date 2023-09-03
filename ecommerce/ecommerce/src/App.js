0a0e4279c471f8d59610dafba4d7b1418ca24192
nikolinaKonatarevic <nikolinaKonatarevic>
2023-09-03 21:48:32 +0200

    links and routes. Files for pages are made

--- ecommerce/ecommerce/src/App.js
+++ ecommerce/ecommerce/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Navbar } from './components/nabvar';
+import {Shop} from "./pages/shop/shop";
+import {Cart} from "./pages/cart/cart";
+import {Contact} from "./pages/contact/contact";
 
 function App() {
   return (
     <div className="App">
-      <div>Nik</div>
+     
+      
+      <Router>
+        <Navbar />
+        <Routes> 
+          
+        <Route path="/" element = {<Shop />}/>
+        <Route path= "/contact" element = {<Contact />} />
+        <Route path="/cart" element = {<Cart />}/>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ ecommerce/ecommerce/src/pages/shop/shop.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Shop = () => {
+    return(
+        <div className="shop">
+            <div className= "shopTitle">   
+                <h1>Welcome to Niki's</h1>
+            </div>
+            <div className="products">
+                
+            </div>
+
+        </div>
+    );
+}
