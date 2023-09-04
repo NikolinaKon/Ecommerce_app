b952f17d5c0c8f212aae2cad8dc498b968132f7e
nikolinaKonatarevic <nikolinaKonatarevic>
2023-09-05 00:15:42 +0200

    changes in CSS, part 1

--- ecommerce/ecommerce/src/pages/contact/contact.jsx
+++ ecommerce/ecommerce/src/pages/contact/contact.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./contact.css";
 
 export const Contact = () => {
     return(
-        <div>Contact</div>
+        <div className="contact">
+            <div className="contactHeadline">Kontakt</div>
+            <div className ="contactInfo"><p>Pozovite nas na +381 609224991</p>
+            <p>Posetite nas na adresi Milutina Milankovća 5, Beograd</p>
+            <p>Pišite nam na nikiskolaci@gmail.com </p>
+            </div>
+        </div>
     );
 }
